b7cacf4e518ef5cd31abe6852c79bf535d43c517
Jeffrey DeVince <jeffrey.devince@gmail.com>
2017-09-15 17:10:19 -0500

    Switching to https

--- frontend/src/app/app-settings.ts
+++ frontend/src/app/app-settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static API_ENDPOINT='http://budgetapi.jeffdevince.com';
+   public static API_ENDPOINT='https://budgetapi.jeffdevince.com';
    //public static API_ENDPOINT='http://localhost:5000';
 }
